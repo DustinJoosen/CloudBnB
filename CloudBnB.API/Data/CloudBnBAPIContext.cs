f17128f03b27d739d9822aa6e702e8c553c45fdd
Dustin Joosen <dustinjoosen2003@gmail.com>
2024-02-12 15:14:05 +0100

    Setup project

---
+++ CloudBnB.API/Data/CloudBnBAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CloudBnB.API.Models;
+
+namespace CloudBnB.API.Data
+{
+    public class CloudBnBAPIContext : DbContext
+    {
+        public CloudBnBAPIContext (DbContextOptions<CloudBnBAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
